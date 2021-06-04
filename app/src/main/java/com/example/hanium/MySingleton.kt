b24238e4581d971c1354bc7637c73e106ee8ed8a
HongSeongKi <tjdrl558@naver.com>
2021-06-04 18:58:48 +0900

    Singleton Setting

---
+++ app/src/main/java/com/example/hanium/MySingleton.kt
@@ -0,0 +1,5 @@
+package com.example.hanium
+
+object MySingleton {
+
+}
---
+++ app/src/main/java/com/example/hanium/User.kt
@@ -0,0 +1,8 @@
+package com.example.hanium
+
+class User {
+    val name:String?=null
+    constructor(name:String){
+
+    }
+}
